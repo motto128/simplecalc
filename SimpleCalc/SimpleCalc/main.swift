e689ce4f86862b22876ca5a60a8af3a72d8c62c6
Joe Motto <joermotto@gmail.com>
2018-01-17 16:42:20 -0800

    finished

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,61 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        //count
+        if args[args.count - 1] == "count" {
+            return args.count - 1;
+        }
+            //average
+        else if args[args.count - 1] == "avg" {
+            var res = 0;
+            for i in args {
+                if i != "avg" {
+                    res += Int(i)!
+                }
+            }
+            if args.count - 1 >= 1 {
+                return res / (args.count - 1)
+            }
+            else {
+                return 0
+            }
+        }
+            //factorial
+        else if args[args.count - 1] == "fact" {
+            var prod = 1
+            if args[0] == "fact" {
+                return 0
+            }
+            else if args[0] == "1" || args[0] == "0" {
+                return prod
+            } else {
+                let num:Int? = Int(args[0])
+                for i in 1...num! {
+                    prod = prod * i
+                }
+                return prod
+            }
+        }
+            //simple operations
+        else {
+            let fNum:Int? = Int(args[0])
+            let op:String = args[1]
+            let sNum:Int? = Int(args[2])
+            
+            if op == "+" {
+                return fNum! + sNum!
+            } else if op == "-" {
+                return fNum! - sNum!
+            } else if op == "/" {
+                return fNum! / sNum!
+            } else if op == "*" {
+                return fNum! * sNum!
+            } else if op == "%" {
+                return fNum! - ((fNum! / sNum! ) * sNum!);
+            }
+        }
+        //any other case
+        return -1
     }
     
     public func calculate(_ arg: String) -> Int {
